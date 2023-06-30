efc2b6d99f313c93d701d91c949d3a09526e152b
Chef Kenji <chef.kenji@bakerfi.xyz>
2023-06-30 22:54:28 +0100

    First Commit

---
+++ hardhat.config.ts
@@ -0,0 +1,55 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { STAGING_ACCOUNTS_PKEYS} from "./constants/test-accounts";
+import {HardhatNetworkAccountUserConfig} from "hardhat/types/config";
+
+const devAccounts: HardhatNetworkAccountUserConfig[] =  STAGING_ACCOUNTS_PKEYS.map(
+  key=>  { return {privateKey: key, balance: "1000000000000000000000000"}}); 
+
+const config: HardhatUserConfig = {
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === "true",
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      accounts: devAccounts,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      gasPrice: 120 * 1000000000,
+      chainId: 1,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 42,
+      gasPrice: 20000000000,
+      gasMultiplier: 2,
+    },
+    matic: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      chainId: 137,            
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      chainId: 80001,      
+      gasMultiplier: 2,
+      accounts: STAGING_ACCOUNTS_PKEYS
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },     
+    ],
+  },
+};
+
+export default config;
