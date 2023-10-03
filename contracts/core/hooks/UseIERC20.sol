af61139d0bf000c42e189c2158ca8940fc0c304a
Chef Kenji <chef.kenji@bakerfi.xyz>
2023-10-03 11:17:37 +0100

    Hooks alocated

--- contracts/core/hooks/UseIERC20.sol
+++ contracts/core/hooks/UseIERC20.sol
@@ -4,17 +4,7 @@ pragma solidity ^0.8.18;
 pragma experimental ABIEncoderV2;
 
 import {ServiceRegistry} from "../ServiceRegistry.sol";
-import {WETH_CONTRACT,UNISWAP_QUOTER, PERCENTAGE_PRECISION,SWAPPER_HANDLER, SETTINGS, WSTETH_ETH_ORACLE, AAVE_V3, FLASH_LENDER, ST_ETH_CONTRACT, WST_ETH_CONTRACT} from "../Constants.sol";
-import {IWETH} from "../../interfaces/tokens/IWETH.sol";
-import {IServiceRegistry} from "../../interfaces/core/IServiceRegistry.sol";
-import {IOracle} from "../../interfaces/core/IOracle.sol";
-import {IWStETH} from "../../interfaces/lido/IWStETH.sol";
-import {IQuoterV2} from "../../interfaces/uniswap/v3/IQuoterV2.sol";
-import {IPoolV3} from "../../interfaces/aave/v3/IPoolV3.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ISwapHandler} from "../../interfaces/core/ISwapHandler.sol";
-import "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
-import {ISettings} from "../../interfaces/core/ISettings.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../../interfaces/aave/v3/DataTypes.sol";
 
