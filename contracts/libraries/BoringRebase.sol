7b73516f731eab714c4d6b6e195a745c981910c4
Chef Kenji <chef.kenji@bakerfi.xyz>
2023-10-10 16:39:26 +0100

    Removed Unused Code

--- contracts/libraries/BoringRebase.sol
+++ contracts/libraries/BoringRebase.sol
@@ -6,7 +6,6 @@ struct Rebase {
     uint256 base;
 }
 
-/// @notice A rebasing library using overflow-/underflow-safe math.
 library RebaseLibrary {
  
     /// @notice Calculates the base value in relationship to `elastic` and `total`.
@@ -39,67 +38,5 @@ library RebaseLibrary {
                 elastic++;
             }
         }
-    }
-
-    /// @notice Add `elastic` to `total` and doubles `total.base`.
-    /// @return (Rebase) The new total.
-    /// @return base in relationship to `elastic`.
-    function add(
-        Rebase memory total,
-        uint256 elastic,
-        bool roundUp
-    ) internal pure returns (Rebase memory, uint256 base) {
-        base = toBase(total, elastic, roundUp);
-        total.elastic += uint128(elastic);
-        total.base += uint128(base);
-        return (total, base);
-    }
-
-    /// @notice Sub `base` from `total` and update `total.elastic`.
-    /// @return (Rebase) The new total.
-    /// @return elastic in relationship to `base`.
-    function sub(
-        Rebase memory total,
-        uint256 base,
-        bool roundUp
-    ) internal pure returns (Rebase memory, uint256 elastic) {
-        elastic = toElastic(total, base, roundUp);
-        total.elastic -= uint128(elastic);
-        total.base -= uint128(base);
-        return (total, elastic);
-    }
-
-    /// @notice Add `elastic` and `base` to `total`.
-    function add(
-        Rebase memory total,
-        uint256 elastic,
-        uint256 base
-    ) internal pure returns (Rebase memory) {
-        total.elastic += uint128(elastic);
-        total.base += uint128(base);
-        return total;
-    }
-
-    /// @notice Subtract `elastic` and `base` to `total`.
-    function sub(
-        Rebase memory total,
-        uint256 elastic,
-        uint256 base
-    ) internal pure returns (Rebase memory) {
-        total.elastic -= uint128(elastic);
-        total.base -= uint128(base);
-        return total;
-    }
-
-    /// @notice Add `elastic` to `total` and update storage.
-    /// @return newElastic Returns updated `elastic`.
-    function addElastic(Rebase storage total, uint256 elastic) internal returns (uint256 newElastic) {
-        newElastic = total.elastic += uint128(elastic);
-    }
-
-    /// @notice Subtract `elastic` from `total` and update storage.
-    /// @return newElastic Returns updated `elastic`.
-    function subElastic(Rebase storage total, uint256 elastic) internal returns (uint256 newElastic) {
-        newElastic = total.elastic -= uint128(elastic);
     }
 }
