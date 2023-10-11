a3d8c0589943e09d31adae60c4bd68f0f6449179
Chef Kenji <chef.kenji@bakerfi.xyz>
2023-10-11 17:07:16 +0100

    BPie

---
+++ contracts/core/BPie.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract BPie is ERC20, Ownable, ERC20Permit, ERC20Votes{
+
+    string private constant _NAME = "Baker Pie";
+    string private constant _SYMBOL = "BPIE";
+    uint256 private immutable _MAX_SUPPLY = 500*1e18; // 500M
+
+    constructor(address initialOwner)
+        ERC20(_NAME, _SYMBOL)
+        Ownable()
+        ERC20Permit(_NAME)
+        ERC20Votes()
+    {
+        _mint(initialOwner, _MAX_SUPPLY);
+        transferOwnership(initialOwner);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
+    }
+}
