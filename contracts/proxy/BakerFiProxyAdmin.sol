fbafa90aee2b325c5e033a1ef19457ec85804318
Helder Vasconcelos <heldervasc@bearstouch.com>
2023-11-29 12:16:55 +0000

    Bakerfi proxy

--- contracts/proxy/BakerFiProxyAdmin.sol
+++ contracts/proxy/BakerFiProxyAdmin.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import { BakerFiProxy } from "./BakerFiProxy.sol";
 
+/**
+ * @title  
+ * @author Chef Kenji <chef.kenji@layerx.xyz>
+ * @author Chef Kal-EL <chef.kal-el@layerx.xyz>
+ * @notice 
+ */
 contract BakerFiProxyAdmin is ProxyAdmin { 
 
     constructor(
