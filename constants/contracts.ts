676403b05849e9f66534c35d8d8e55d7d783f202
Chef Kenji <chef.kenji@bakerfi.xyz>
2023-12-05 15:39:06 +0000

    Alpha 3 Contract Addresses (#6)
    * new Contracts
    
    * Working

--- constants/contracts.ts
+++ constants/contracts.ts
@@ -45,22 +45,23 @@ export const deployConfigMap : {[key: string]: DeployConfig} =
         vaultProxy: "0xb99b2F8f3d121f2B491Cc61b84689a5638E106B4",        
     },
     "arbitrum": {
-        serviceRegistry: "0xd68a2846168052C43Ae44591B6F67588D218b150",       
+        proxyAdmin: "0xa641256b225e215c559cdC58F8757ad7140A2723",
+        serviceRegistry: "0x82405993C4473A4364DF9D0C09E1A81Ecef9CA25",       
         weth: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",  
         stETH: "0x07C1F45Dc0a620E6716d8A45485B8f0A79E270F8",
         wstETH: "0x5979D7b546E38E414F7E9822514be443A4800529",
-        flashLender: "0x38BEB36E9E3cF01085D0e8C317285eeefF126e7f",
+        flashLender: "0x2db37f9DcD838B06a40Dc1aB171a8eBB474ef44C",
         uniswapRouter: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45", 
         uniswapQuoter: "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",
         AAVEv3Pool: "0x794a61358D6845594F94dc1DB02A252b5b4814aD", 
-        wstETHETHOracle: "0x8D5EEF9bcbB008784b6232b61890F52A7648d81F",
-        ethUSDOracle: "0x6060608FD42b2bBC23F21ff7BB4fE72F3Ca21Ef1",
-        strategy: "0x6c59eD8e7B8641f7Abe89B1dBa8C5847799FD6FB",
-        strategyProxy: "0x6c59eD8e7B8641f7Abe89B1dBa8C5847799FD6FB",
-        vault: "0x7454072e99Bb7f7079fe24BFDf32A3F458a35d3C",      
-        vaultProxy: "0x7454072e99Bb7f7079fe24BFDf32A3F458a35d3C",          
-        settings: "0xb5A087ba08a6ee489c6f554Ca458741A18CDae99",
-        settingsProxy: "0xb5A087ba08a6ee489c6f554Ca458741A18CDae99"            
+        wstETHETHOracle: "0x63121b44680C502FDeDC725cd7CBcD37E0967d88",
+        ethUSDOracle: "0xd0fDAfd2cf24fC4D8B57ffC001d3DC10422d3623",
+        strategy: "0x8Fb35FEf7c2fcdfE09b6985f894905b875111a72",
+        strategyProxy: "0x40aB23988835bdA372deA30690CCCC3419580548",
+        vault: "0xA804f7dB9Df7C185a3Ea9221b70E655858B93a81",      
+        vaultProxy: "0x5c1b2312FaE6c0d61B6A15A8093842E9fE5b1e44",          
+        settings: "0x4a5c5FDf4A42AA62945D037DCc636Bded4ea6bf1",
+        settingsProxy: "0xBd7f910A074D9d35789a47FF0962b5706D7855dF"            
     }
 }
 
