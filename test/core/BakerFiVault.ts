1736f711c6dc9abb2032def79fb7bc49893a9bcf
Helder Vasconcelos <heldervasc@bearstouch.com>
2024-03-19 14:33:08 +0000

    BakerFi Vault tests with outdated prices

--- test/core/BakerFiVault.ts
+++ test/core/BakerFiVault.ts
@@ -295,16 +295,96 @@ describeif(network.name === "hardhat")("BakerFi Vault For L2s", function () {
     const { settings, vault, owner} = await loadFixture(deployFunction);
 
     // Price Max Age 10 Hours
-    await settings.setPriceMaxAge(36000);
-   
-    // advance time by one hour and mine a new block
-    await time.increase(3600);
-
+    await settings.setPriceMaxAge(360);
     const tx =  await vault.deposit(owner.address, {
       value: ethers.parseUnits("10", 18),
     });
+
+
+    // advance time by one hour and mine a new block
+    await time.increase(3600);
+
+
     await expect(tx)
       .to.changeEtherBalances([owner.address], [ethers.parseUnits("-10", 18)]);   
-  } )
+  })
+
+
+
+  it("convertToShares should return with outdated prices",async ()=> { 
+
+    const { settings, vault, owner} = await loadFixture(deployFunction);
+
+    await settings.setPriceMaxAge(360);
+    
+    await vault.deposit(owner.address, {
+      value: ethers.parseUnits("10", 18),
+    });
+
+    // advance time by one hour and mine a new block
+    await time.increase(3600);
+
+    expect(await vault.convertToShares(ethers.parseUnits("1", 18))).to.equal(
+      1000000000000000000n
+    );
+  })
+
+  it("convertToAssets should return with outdated prices",async ()=> { 
+
+    const { settings, vault, owner} = await loadFixture(deployFunction);
+
+    // Price Max Age 10 Hours
+    await settings.setPriceMaxAge(360);
+   
+    await vault.deposit(owner.address, {
+      value: ethers.parseUnits("10", 18),
+    });
+
+    // advance time by one hour and mine a new block
+    await time.increase(3600);
+
+    expect(await vault.convertToAssets(ethers.parseUnits("1", 18))).to.equal(
+      1000000000000000000n
+    );
+  })
+
+  it("tokenPerETH should return with outdated prices",async ()=> { 
+
+    const { settings, vault, owner} = await loadFixture(deployFunction);
+  
+    // Price Max Age 10 Hours
+    await settings.setPriceMaxAge(360);
+
+    await vault.deposit(owner.address, {
+      value: ethers.parseUnits("10", 18),
+    });
+   
+    // advance time by one hour and mine a new block
+    await time.increase(3600);
+    
+    expect(await vault.tokenPerETH()).to.equal(
+      1000000000000000000n
+    );
+  })
+
+  it("totalAssets should return with outdated prices",async ()=> { 
+
+    const { settings, vault, owner} = await loadFixture(deployFunction);
+  
+    // Price Max Age 10 Hours
+    await settings.setPriceMaxAge(360);
+
+    await vault.deposit(owner.address, {
+      value: ethers.parseUnits("10", 18),
+    });
+   
+    // advance time by one hour and mine a new block
+    await time.increase(3600);
+    
+    expect(await vault.totalAssets()).to.equal(
+      9962113816060668112n
+    );
+  })
+
 
 });
