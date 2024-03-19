b5a29f79f8596a44119d45d29848b3b74332e95e
Helder Vasconcelos <heldervasc@bearstouch.com>
2024-03-19 16:01:30 +0000

    Vault Proxy updated

--- test/proxy/VaultProxy.ts
+++ test/proxy/VaultProxy.ts
@@ -4,71 +4,9 @@ import { ethers, network } from "hardhat";
 import { describeif } from "../common";
 import {
     deployServiceRegistry,
-    deployVault,
-    deployStEth,
-    deployWStEth,
-    deployAaveV3,
-    deployFlashLender,
-    deployOracleMock,
-    deployWETH,
-    deployStrategyAAVEv3WstETH,
-    deploySettings,
-    deployQuoterV2Mock,
-  } from "../../scripts/common";
+} from "../../scripts/common";
 
 describeif(network.name === "hardhat")("Vault Proxy", function () {
-  
-  async function deployFunction() {
-    const [deployer, otherAccount] = await ethers.getSigners();
-
-    // Deploy Proxy Admin
-    const BakerFiProxyAdmin = await ethers.getContractFactory("BakerFiProxyAdmin");
-    const proxyAdmin = await BakerFiProxyAdmin.deploy(deployer.address);
-    await proxyAdmin.waitForDeployment();    
-    
-    // Deploy Service Registry
-    const serviceRegistry = await deployServiceRegistry(deployer.address);
-
-    // Deploy Strategy Mock
-    const StrategyMock = await ethers.getContractFactory("StrategyMock");
-    const strategy = await StrategyMock.deploy();
-    await strategy.waitForDeployment();
-    
-    // Deploy Non Upgradable Settings 
-    const Settings = await ethers.getContractFactory("Settings");
-    const settings = await Settings.deploy();
-    await settings.waitForDeployment();    
-    await serviceRegistry.registerService(
-        ethers.keccak256(Buffer.from("Settings")),
-        await settings.getAddress()
-    );
-        
-    // Deploy Vault Default Implementation
-    const Vault = await ethers.getContractFactory("Vault");
-    const vault = await Vault.deploy();
-    await vault.waitForDeployment();
-
-    // Vault Proxy Deployment 
-    const BakerFiProxy = await ethers.getContractFactory("BakerFiProxy");
-    const proxyDeployment = await BakerFiProxy.deploy(
-      await vault.getAddress(),
-      await proxyAdmin.getAddress(),
-      Vault.interface.encodeFunctionData("initialize", [
-        deployer.address,
-        "Bread ETH",
-        "brETH",
-        await serviceRegistry.getAddress(),
-        await strategy.getAddress()
-      ])
-    );
-    
-    await proxyDeployment.waitForDeployment();    
-
-    const vaultProxy = await Vault.attach(await proxyDeployment.getAddress());
-
-    return { deployer, settings, vaultProxy, proxyAdmin, otherAccount, proxyDeployment};
-  }
-
 
   it("Vault Initialization", async function () {
     const { vaultProxy, deployer } = await loadFixture(deployFunction);    
@@ -77,6 +15,56 @@ describeif(network.name === "hardhat")("Vault Proxy", function () {
     expect(await vaultProxy.totalSupply()).to.equal(0);
     expect(await vaultProxy.owner()).to.equal(deployer.address);
   })
-
   
 });
+
+async function deployFunction() {
+  const [deployer, otherAccount] = await ethers.getSigners();
+
+  // Deploy Proxy Admin
+  const BakerFiProxyAdmin = await ethers.getContractFactory("BakerFiProxyAdmin");
+  const proxyAdmin = await BakerFiProxyAdmin.deploy(deployer.address);
+  await proxyAdmin.waitForDeployment();    
+  
+  // Deploy Service Registry
+  const serviceRegistry = await deployServiceRegistry(deployer.address);
+
+  // Deploy Strategy Mock
+  const StrategyMock = await ethers.getContractFactory("StrategyMock");
+  const strategy = await StrategyMock.deploy();
+  await strategy.waitForDeployment();
+  
+  // Deploy Non Upgradable Settings 
+  const Settings = await ethers.getContractFactory("Settings");
+  const settings = await Settings.deploy();
+  await settings.waitForDeployment();    
+  await serviceRegistry.registerService(
+      ethers.keccak256(Buffer.from("Settings")),
+      await settings.getAddress()
+  );
+      
+  // Deploy Vault Default Implementation
+  const Vault = await ethers.getContractFactory("Vault");
+  const vault = await Vault.deploy();
+  await vault.waitForDeployment();
+
+  // Vault Proxy Deployment 
+  const BakerFiProxy = await ethers.getContractFactory("BakerFiProxy");
+  const proxyDeployment = await BakerFiProxy.deploy(
+    await vault.getAddress(),
+    await proxyAdmin.getAddress(),
+    Vault.interface.encodeFunctionData("initialize", [
+      deployer.address,
+      "Bread ETH",
+      "brETH",
+      await serviceRegistry.getAddress(),
+      await strategy.getAddress()
+    ])
+  );
+  
+  await proxyDeployment.waitForDeployment();    
+
+  const vaultProxy = await Vault.attach(await proxyDeployment.getAddress());
+
+  return { deployer, settings, vaultProxy, proxyAdmin, otherAccount, proxyDeployment};
+}
