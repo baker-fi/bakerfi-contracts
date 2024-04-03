65647e1b20dbf1e1866eb821f8bc7eeb8795e5ed
Helder Vasconcelos <heldervasc@bearstouch.com>
2024-04-03 14:59:59 +0100

    Right license

--- contracts/core/governance/BakerFiGovernor.sol
+++ contracts/core/governance/BakerFiGovernor.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.18;
 
 import {Governor, IGovernor, IERC165} from "@openzeppelin/contracts/governance/Governor.sol";
