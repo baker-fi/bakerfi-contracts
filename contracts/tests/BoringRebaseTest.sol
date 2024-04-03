e494f450b92e071e1da6e1bccd20f0a4281feca1
Helder Vasconcelos <heldervasc@bearstouch.com>
2024-04-03 12:02:38 +0100

    Renamed Library

--- contracts/tests/BoringRebaseTest.sol
+++ contracts/tests/BoringRebaseTest.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-import {Rebase, RebaseLibrary} from "../libraries/BoringRebase.sol";
+import {Rebase, RebaseLibrary} from "../libraries/RebaseLibrary.sol";
 
 contract TestRebaseLibrary {
     using RebaseLibrary for Rebase;
