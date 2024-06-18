f77d60e4bf191abcc29795dd8e4c70eb4feba685
Chef Kenji <chef.kenji@bakerfi.xyz>
2024-06-18 14:43:07 +0100

    [BKR-68] Returning ETH excess after a pyth price update (#53)
    * Working
    
    * Returing ETH Excesss
    
    * Excess

--- contracts/oracles/PythOracle.sol
+++ contracts/oracles/PythOracle.sol
@@ -65,6 +65,10 @@ contract PythOracle is IOracle {
         uint256 fee = _pyth.getUpdateFee(priceUpdates);
         if (msg.value < fee) revert NoEnoughFee();
         _pyth.updatePriceFeeds{value: fee}(priceUpdates);
+        uint256 excessETH = msg.value - fee;
+        if(excessETH != 0) {
+            payable(msg.sender).transfer(excessETH);
+        }
         return _getPriceInternal(0);
     }
 
