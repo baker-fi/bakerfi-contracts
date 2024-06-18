c2a9beebceb9cd7c0894cd56fdb2b07115614cfa
Helder Vasconcelos <heldervasc@bearstouch.com>
2024-06-18 11:36:56 +0100

    Fixes

--- test/core/VaultWstETH.ts
+++ test/core/VaultWstETH.ts
@@ -172,7 +172,7 @@ describeif(network.name === "hardhat")(
     });
 
     it("Withdraw With Service Fees", async function () {
-      const { owner, vault, settings, otherAccount, anotherAccount } =
+      const { owner, vault, settings, otherAccount } =
         await loadFixture(deployFunction);
 
       const provider = ethers.provider;
@@ -232,10 +232,8 @@ describeif(network.name === "hardhat")(
       const {
         owner,
         vault,
-        strategy,
-        otherAccount,
-        flashLender,
-        anotherAccount,
+        strategy,        
+        flashLender,        
       } = await loadFixture(deployFunction);
 
       await flashLender.setFlashLoanFee(0);
@@ -285,7 +283,6 @@ describeif(network.name === "hardhat")(
         vault,
         wstETH,
         weth,
-        settings,
         strategy,
         oracle,
         aave3Pool,
@@ -516,7 +513,6 @@ describeif(network.name === "hardhat")(
  */
 async function deployFunction() {
   const networkName = network.name;
-  const chainId = network.config.chainId;
   const config = BaseConfig[networkName];
   const [owner, otherAccount, anotherAccount] = await ethers.getSigners();
   const STETH_MAX_SUPPLY = ethers.parseUnits("1000010000", 18);
@@ -639,7 +635,7 @@ async function deployFunction() {
 }
 
 async function deployWithMockStrategyFunction() {
-  const [owner, otherAccount, anotherAccount] = await ethers.getSigners();
+  const [owner, otherAccount ] = await ethers.getSigners();
   const BakerFiProxyAdmin = await ethers.getContractFactory(
     "BakerFiProxyAdmin"
   );
