c2e05d87afd3c6219edfdd4c80fc91d316039cef
Chef Kenji <chef.kenji@bakerfi.xyz>
2024-06-28 10:27:52 +0100

    Pyth Feed Ids Organized (#62)
    * Pyth Feeds
    
    * Feed ids
    
    * Pyth Feed ids

---
+++ constants/pyth.ts
@@ -0,0 +1,22 @@
+
+export enum PythFeedNameEnum {
+    ETH_USD = 'ETH/USD',
+    WSTETH_USD = 'WSTETH/USD',
+    CBETH_USD = 'CBTETH/USD',
+};
+
+export type FeedNameEnumType =  
+    PythFeedNameEnum.ETH_USD |
+    PythFeedNameEnum.WSTETH_USD | 
+    PythFeedNameEnum.CBETH_USD;
+
+export type FeedsDictionary = { [key: string]: string };
+
+export const feedIds: FeedsDictionary = {
+    [PythFeedNameEnum.ETH_USD]: 
+        '0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace',
+    [PythFeedNameEnum.WSTETH_USD]: 
+        '0x6df640f3b8963d8f8358f791f352b8364513f6ab1cca5ed3f1f7b5448980e784',
+    [PythFeedNameEnum.CBETH_USD]: 
+        '0x15ecddd26d49e1a8f1de9376ebebc03916ede873447c1255d2d5891b92ce5717',
+}
