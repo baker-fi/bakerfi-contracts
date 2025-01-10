e80920956980ce24c4551a960739280f90cd775c
Chef Kenji <chef.kenji@bakerfi.xyz>
2025-01-10 15:43:46 +0000

    AssetsMax Fixed (#22)

--- test/core/strategies/StrategySupplyMorpho.ts
+++ test/core/strategies/StrategySupplyMorpho.ts
@@ -18,8 +18,8 @@ describeif(network.name === 'base_devnet')('Strategy Supply Morpho', function ()
     const weth = WETH.attach(config.weth);
     const WSETH_ETH_Market = '0x3a4048c64ba1b375330d376b1ce40e4047d03b47ab4d48af484edec9fec801ba';
 
-    // 2. Deposit 10 ETH to WETH
-    const deployAmount = ethers.parseEther('10'); // 10 ETH in Wei
+    // 2. Deposit 100 ETH to WETH
+    const deployAmount = ethers.parseEther('100'); // 100 ETH in Wei
     await weth.deposit({ value: deployAmount });
 
     // 3. Deploy StrategySupplyMorpho
@@ -29,16 +29,15 @@ describeif(network.name === 'base_devnet')('Strategy Supply Morpho', function ()
       config.morpho,
       WSETH_ETH_Market,
     );
+
     await morphoSupply.waitForDeployment();
-    await weth.approve(await morphoSupply.getAddress(), deployAmount);
+    await weth.approve(await morphoSupply.getAddress(), ethers.MaxUint256);
 
     return { morphoSupply, weth, config, owner };
   }
 
   it('Vault Initialization', async function () {
     const { morphoSupply, config } = await loadFixture(deployFunction);
-    const deployAmount = ethers.parseEther('10');
-    // 5. Deploy 10 WETH to StrategySupplyMorpho
     expect(await morphoSupply.asset()).to.equal(config.weth);
     // Verify if the deposit is reflected in the strategy
     expect(await morphoSupply.totalAssets()).to.equal(0);
@@ -46,6 +45,7 @@ describeif(network.name === 'base_devnet')('Strategy Supply Morpho', function ()
 
   it('Deposit 10 WETH', async function () {
     const { morphoSupply } = await loadFixture(deployFunction);
+    // Deploy 10 WETH to StrategySupplyMorpho
     const deployAmount = ethers.parseEther('10');
     // 5. Deploy 10 WETH to StrategySupplyMorpho
     expect(await morphoSupply.deploy(deployAmount))
